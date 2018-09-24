fec5144d8600e5cf1eadacc2548a7225f79e1eb7
Axolotl <samuelmachat@gmail.com>
2018-09-23 20:13:21 -0400

    added plugin gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,12 +13,12 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        //icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
