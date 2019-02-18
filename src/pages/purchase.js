e848520a5d45df0a6478908e71b5924262b6fa19
Axolotl <samuelmachat@gmail.com>
2019-02-17 22:03:47 -0500

    changes to purchase page

--- src/pages/purchase.js
+++ src/pages/purchase.js
@@ -1,32 +1,16 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { P } from '../styles/Text'
+import { CTAButton } from '../styles/IndexPage'
+import { Wrapper, Text } from '../styles/Purchase'
 
 const Purchase = () => (
   <Layout>
-    <P>Purchase.</P>
-    <form
-      action="https://www.paypal.com/cgi-bin/webscr"
-      method="post"
-      target="_top"
-    >
-      <input type="hidden" name="cmd" value="_s-xclick" />
-      <input type="hidden" name="hosted_button_id" value="EEDRXML5A9UJQ" />
-      <input
-        type="image"
-        src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif"
-        border="0"
-        name="submit"
-        alt="PayPal - The safer, easier way to pay online!"
-      />
-      <img
-        alt=""
-        border="0"
-        src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-        width="1"
-        height="1"
-      />
-    </form>
+    <Wrapper>
+      <Text>Available through Amazon.</Text>
+      <Text>Retail: $12.99</Text>
+      <CTAButton>Purchase now</CTAButton>
+    </Wrapper>
   </Layout>
 )
 
---
+++ src/styles/Purchase.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`
+
+export const Text = styled.p`
+  font-size: 2rem;
+  font-family: 'Oxygen', sans-serif;
+  color: #363636;
+  margin: 2rem 0 1.5rem;
+`
