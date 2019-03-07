3ee53bcf7ad60733130347642ca298699f88f50f
Axolotl <samuelmachat@gmail.com>
2019-03-06 23:12:15 -0500

    changes to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { Wrapper, InnerWrapper, Text } from '../styles/Contact'
+import {
+  Wrapper,
+  InnerWrapper,
+  Text,
+  Header,
+  TextBox,
+  TextWrapper,
+} from '../styles/Contact'
 
 const Contact = () => (
   <Layout>
     <Wrapper>
       <InnerWrapper>
-        <Text>PO Box 179.</Text>
-        <Text>Batesville, Va 22924</Text>
-        <Text>Info@whimseebooks.com</Text>
+        <Header>Questions? Comments? We are here for you!</Header>
+        <TextBox>
+          <TextWrapper>
+            <Text>PO Box 179.</Text>
+            <Text>Batesville, Va 22924</Text>
+            <Text>
+              <a href="mailto:Info@whimseebooks.com">Info@whimseebooks.com</a>{' '}
+            </Text>
+          </TextWrapper>
+        </TextBox>
       </InnerWrapper>
     </Wrapper>
   </Layout>
--- src/styles/Contact.js
+++ src/styles/Contact.js
@@ -18,3 +18,18 @@ export const Text = styled.p`
   color: #363636;
   margin: 2rem 0 1.5rem;
 `
+
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+`
+
+export const TextWrapper = styled.div``
+
+export const Header = styled.h2`
+  font-family: 'Henny Penny', cursive;
+  font-size: 2.6rem;
+  color: #ff1e00;
+`
