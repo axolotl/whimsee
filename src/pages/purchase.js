e296f562aa0ef07bf0dd4311dc8dee78c716d1f4
Axolotl <samuelmachat@gmail.com>
2019-03-25 22:28:23 -0400

    add purchase link

--- src/pages/purchase.js
+++ src/pages/purchase.js
@@ -8,7 +8,10 @@ const Purchase = () => (
     <Wrapper>
       <Text>Available through Amazon.</Text>
       <Text>Retail: $12.99</Text>
-      <CTAButton>Purchase now</CTAButton>
+
+      <a href="https://www.amazon.com/gp/product/099913101X/ref=ox_sc_saved_title_1?smid=A35KY9GH64EZOE&psc=1">
+        <CTAButton>Purchase now</CTAButton>
+      </a>
     </Wrapper>
   </Layout>
 )
